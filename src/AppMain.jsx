3106ade372300aa17f3da0304239c17ce329995d
MD Tanjimul Islam <67940292+tnr-tanjimul@users.noreply.github.com>
2021-12-17 12:08:05 +0600

    update tittle

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,12 +1,11 @@
+import { BrowserRouter as Router, Route, Switch,useParams } from 'react-router-dom'
 import LeftSideBar from "./Components/LeftSidebar";
-import Dashboard from "./Components/Pages/Dashboard";
 import RightSidebar from "./Components/RightSidebar";
 import TopBar from "./Components/TopBar";
-import { BrowserRouter as Router, Route, Switch,useParams } from 'react-router-dom'
-
-import Contact from "./Components/Pages/Contact";
 import AuthLogin from "./Components/Auth/AuthLogin";
 import AuthRegistration from "./Components/Auth/AuthRegistration";
+import Dashboard from "./Components/Pages/Dashboard";
+import Contact from "./Components/Pages/Contact";
 import GroupCreate from "./Components/Pages/GroupCreate";
 import GroupView from "./Components/Pages/GroupView";
 import ContactView from "./Components/Pages/ContactView";
