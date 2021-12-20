a27527b24cb6ed18893822482fd37027895a217f
MD Tanjimul Islam <67940292+tnr-tanjimul@users.noreply.github.com>
2021-12-20 21:32:19 +0600

    Update AuthLogin.jsx

--- src/Components/Auth/AuthLogin.jsx
+++ src/Components/Auth/AuthLogin.jsx
@@ -2,7 +2,45 @@ import AuthLogo from "../AuthLogo";
 import AuthQuotes from "../AuthQuotes";
 import AuthSocialButton from "../AuthSocialButton";
 import { Link } from "react-router-dom";
-function AuthLogin() {
+import axios from "axios";
+import React, {useState, userEffect} from "react";
+import { useHistory } from "react-router-dom";
+const AuthLogin = ()=>{
+    let[token, setToken]= useState("");
+    let[name, setName] = useState("");
+    let[password, setPassword] =useState("");
+
+    let history = useHistory();
+    function handleRedirect() {
+        history.push("/");
+    }
+
+    const loginSubmit= ()=>{
+        var obj = {Email: name, Password: password};
+        axios.post("login",obj)
+        .then(resp=>{
+
+            if(resp.data.status === 401){
+                alert(resp.data.message);
+            }
+            else{
+                var token = resp.data;
+                console.log(token);
+                var user = {UserId: token.UserId, access_token:token.Token1,role:token.Role,tokenId:token.Id};
+                localStorage.setItem('user',JSON.stringify(user));
+                handleRedirect();
+            }
+            
+            
+            // console.log(localStorage.getItem('user'));
+        }).catch(err=>{
+            console.log(err);
+            alert(err);
+
+        });
+
+
+    }
     return (
 
         <div className="auth-fluid">
@@ -19,10 +57,9 @@ function AuthLogin() {
                         <p className="text-muted mb-4">Enter your email address and password to access account.</p>
 
                         {/**{/* form */}
-                        <form action="/Auth/Login" method="post">
+                        <div>
 
-                            <input name="__RequestVerificationToken" type="hidden" defaultValue="mSs95QLde_MdW6VUPnkgIWAgAUuoTuJ_NrplrDZdU8k_ySrlVEMdyLrXGDNBJSsbhwaBgd_OXYNahIpceRa_PbMv3MVLJG__jKoknMlwUWE1" />
-
+                         
                             <div className="form-group">
 
 
@@ -32,20 +69,20 @@ function AuthLogin() {
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Email">Email address</label>
-                                <input className="form-control" type="text" name="Email" id="Email" required="" placeholder="Enter your email" />
-                                <span className="field-validation-valid text-danger" data-valmsg-htmlhtmlFor="Email" data-valmsg-replace="true"></span>
+                                <input className="form-control" type="text" value={name} onChange={(e)=>setName(e.target.value)} name="Email" id="Email" required="" placeholder="Enter your email" />
+                                <span className="field-validation-valid text-danger" data-valmsg-htmlhtmlfor="Email" data-valmsg-replace="true"></span>
                             </div>
                             <div className="form-group">
                                 <Link to="auth-recoverpw-2.php" className="text-muted float-right"><small>Forgot your password?</small></Link>
                                 <label htmlFor="Password">Password</label>
                                 <div className="input-group input-group-merge">
-                                    <input type="password" name="Password" id="Password" className="form-control" placeholder="Enter your password" />
+                                    <input type="password" name="Password" id="Password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="Enter your password" />
                                     <div className="input-group-append" data-password="false">
                                         <div className="input-group-text">
                                             <span className="password-eye"></span>
                                         </div>
                                     </div>
-                                    <span className="field-validation-valid text-danger" data-valmsg-htmlhtmlFor="Password" data-valmsg-replace="true"></span>
+                                    <span className="field-validation-valid text-danger" data-valmsg-htmlhtmlfor="Password" data-valmsg-replace="true"></span>
                                 </div>
                             </div>
 
@@ -56,15 +93,15 @@ function AuthLogin() {
                                 </div>
                             </div>
                             <div className="form-group mb-0 text-center">
-                                <button className="btn btn-primary btn-block" type="submit">Log In </button>
+                                <button className="btn btn-primary btn-block" onClick={loginSubmit}>Log In </button>
                             </div>
                             {/** {/* social*/}
                             <AuthSocialButton></AuthSocialButton>
-                        </form>
+                        </div>
                         {/**{/* end form*/}
                         {/* Footer*/}
                         <footer className="footer footer-alt">
-                            <p className="text-muted">Don't have an account? <Link to="/Auth/Registration" className="text-muted ml-1"><b>Sign Up</b></Link></p>
+                            <p className="text-muted">Don't have an account? <Link to="/register" className="text-muted ml-1"><b>Sign Up</b></Link></p>
                         </footer>
 
                     </div> {/* end .card-body */}
@@ -74,7 +111,7 @@ function AuthLogin() {
             <AuthQuotes></AuthQuotes>
         </div>
 
-    );
+    )
 }
 
 export default AuthLogin;
